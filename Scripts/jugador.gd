d71bb58cdb4589e1fe2cd8a564dc7482b1fe827b
Baly333 <Balayla.ceres.alex@alumnes.fundaciocic.org>
2025-04-08 10:00:40 +0200

    1
    1

--- Scripts/jugador.gd
+++ Scripts/jugador.gd
@@ -1,9 +1,17 @@
 extends CharacterBody2D
 var velocitat := 200
-var direccio := Vector2.ZERO
+var direccio := 0
 func _ready() -> void:
-	position = Vector2(-105,412)
+	position = Vector2(-92,386)
 func _process(delta: float) -> void:
-	direccio = Input.get_vector("moure_esquerra","moure_dreta","moure_amunt","moure_avall")
-	velocity = direccio * velocitat
+	direccio = Input.get_axis("moure_esquerra","moure_dreta")
+	velocity = Vector2 (direccio * velocitat, 0)
 	move_and_slide()
+
+
+ 
+	
+
+
+func _on_obstacle_body_entered(body: Node2D) -> void:
+	$obstacle._disapear()
--- Scripts/obstacle.gd
+++ Scripts/obstacle.gd
@@ -1,7 +1,10 @@
-extends CharacterBody2D
-var gravetat := Vector2.DOWN * 980
+extends Area2D
+var gravetat := Vector2.DOWN * 400
+var direccio := Vector2.ZERO
 func _ready() -> void:
 	position = Vector2(-347, -161.0)
 func _process(delta: float) -> void:
-	velocity += gravetat * delta
-	move_and_slide()
+	position += gravetat * delta
+func _disapear() -> void:
+	print("1")
+	visible = false
